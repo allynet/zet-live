15c5574d9c6a11c756aa35e4d8a4ecd857ea2b4f
Josip Igrec <josip@igr.ec>
2025-07-22 17:34:12 +0200

    Add movement arrows to vehicles

--- frontend/src/app/entity/v1/vehicle.ts
+++ frontend/src/app/entity/v1/vehicle.ts
@@ -8,6 +8,7 @@ export class VehicleV1<
   tripId: string;
   lat: number;
   lng: number;
+  moveAngle?: number;
   mapEntity = undefined as TMapEntity;
 
   public constructor(data: {
@@ -16,12 +17,14 @@ export class VehicleV1<
     tripId: string;
     latitude: number;
     longitude: number;
+    moveAngle?: number;
   }) {
     this.id = data.id;
     this.routeId = data.routeId;
     this.tripId = data.tripId;
     this.lat = data.latitude;
     this.lng = data.longitude;
+    this.moveAngle = data.moveAngle;
   }
 
   public static fromSimple(data: (string | number)[]) {
@@ -45,6 +48,7 @@ export class VehicleV1<
       tripId: this.tripId,
       latitude: this.lat,
       longitude: this.lng,
+      moveAngle: this.moveAngle,
     };
   }
 
@@ -63,6 +67,22 @@ export class VehicleV1<
     map: MaplibreglMap,
     createMapMarkerElement: (vehicle: VehicleV1) => HTMLElement
   ) {
+    {
+      const prevLoc = this.mapEntity?.getLngLat();
+      if (prevLoc) {
+        const hasMoved =
+          Math.abs(this.lat - prevLoc.lat) > 0 ||
+          Math.abs(this.lng - prevLoc.lng) > 0;
+
+        if (hasMoved) {
+          this.moveAngle = Math.atan2(
+            this.lat - prevLoc.lat,
+            this.lng - prevLoc.lng
+          );
+        }
+      }
+    }
+
     let entity =
       this.mapEntity ??
       new MaplibreglMarker({
@@ -70,6 +90,14 @@ export class VehicleV1<
       });
 
     entity = entity.setLngLat(this).addTo(map);
+
+    if (this.moveAngle) {
+      entity
+        .getElement()
+        .style.setProperty("--move-angle", this.moveAngle.toString());
+    } else {
+      entity.getElement().style.removeProperty("--move-angle");
+    }
 
     return this.setMapEntity(entity);
   }
